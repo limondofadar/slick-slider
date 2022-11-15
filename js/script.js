f950df31fdeff172b04b45af56e0734af0346640
LIMON <115656384+limondofadar@users.noreply.github.com>
2022-11-15 18:34:36 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// jquery syntax 
+$(function(){
+
+
+
+
+    $('.slider_part').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed:1500,
+        arrows:true,
+        prevArrow:'<i class="fa-solid fa-arrow-left-long prevArrow"></i>',
+        nextArrow:'<i class="fa-solid fa-arrow-right-long nextArrow"></i>',
+        dots:true,
+       
+       
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
